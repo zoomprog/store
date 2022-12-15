116f9d1c157fa91001f4c9b75382ee9336cfc2d2
zoomka <rrarrk123@gmail.com>
2022-12-16 01:24:00 +0300

    Create bd. Add products. edit code

--- store/urls.py
+++ store/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from products.views import index
 from products.views import products
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products')
-
-
+    path('products/', products, name='products'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
