6e4e51c492f453dbd36e48c98d9b1f4a00b08d21
zoomka <rrarrk123@gmail.com>
2022-12-15 16:20:39 +0300

    edit code

--- products/views.py
+++ products/views.py
@@ -10,24 +10,24 @@ def index(request):
 
 def products(request):
     contex = {'title': 'Store - Каталог',
-              'products': [{'image':'/static/vendor/img/products/Adidas-hoodie.png',
-                            'name':'Худи черного цвета с монограммами adidas Originals',
-                            'price:': 6090,
+              'products': [{'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                            'name': 'Худи черного цвета с монограммами adidas Originals',
+                            'price': 6090,
                             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
                             },
 
                            {'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
                             'name': 'Синяя куртка The North Face',
-                            'price:': 23725,
+                            'price': 23725,
                             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
                             },
 
                            {'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
                             'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                            'price:': 3390,
+                            'price': 3390,
                             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'
                             },
 
                            ]
               }
-    return render(request, 'products/products.html',contex)
+    return render(request, 'products/products.html', contex)
