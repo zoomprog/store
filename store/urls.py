b43d0d7640046b36c25f75b91408f0a5dbadb592
zoomka <rrarrk123@gmail.com>
2022-12-16 12:43:15 +0300

    edit products url

--- store/urls.py
+++ store/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from products.views import index
 from products.views import products
 from django.conf.urls.static import static
@@ -23,7 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
 ]
 
 if settings.DEBUG:
